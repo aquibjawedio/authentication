147b65e667a73b6beb1df1515837e58ee1c304a7
Aquib Jawed <aquibjawed.dev@gmail.com>
2025-07-07 14:47:09 +0530

    feat: logger added

--- backend/src/controllers/health.controller.js
+++ backend/src/controllers/health.controller.js
@@ -1,5 +1,6 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import { logger } from "../utils/logger.js";
 
 const healthController = asyncHandler(async (req, res) => {
   const healthData = {
@@ -8,6 +9,7 @@ const healthController = asyncHandler(async (req, res) => {
     timestamp: new Date().toISOString(),
   };
 
+  logger.info("Sever is running", healthData);
   return res
     .status(200)
     .json(new ApiResponse(200, "Healthcheck Passed", healthData));
